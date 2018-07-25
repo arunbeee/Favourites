7251664d9e5573597a45175fb479fa4a738043b0
Arun Balakrishnan <arun.therocker@gmail.com>
2018-07-25 22:54:08 +0530

    Initial Commit

---
+++ Favourites/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favourites
+//
+//  Created by Arun Balakrishnan on 25/07/18.
+//  Copyright © 2018 Arun Balakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
